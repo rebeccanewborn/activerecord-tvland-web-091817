873dbb8a4248b4e1218f4e4f401095047eec4928
rebeccanewborn <rjn58@cornell.edu>
2017-09-28 17:30:15 -0400

    done

--- config/environment.rb
+++ config/environment.rb
@@ -8,3 +8,6 @@ Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| requir
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
+
+old_logger = ActiveRecord::Base.logger
+ActiveRecord::Base.logger = nil
